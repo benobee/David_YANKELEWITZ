10238052d380528f3b489c62a2f2ea0182cca836
Ben Obee <benobee@gmail.com>
2017-02-16 17:11:15 -0800

    tweaks

--- source/core/controller.js
+++ source/core/controller.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import 'owl.carousel';
 import { TweenMax, Power2, TimelineLite } from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 
@@ -8,7 +7,6 @@ const controller = {
 		this.getListData();
 		this.events();
 		this.indexArray();
-		this.projectPage();
 		this.navSmoothScroll();
 	},
 	navSmoothScroll() {
@@ -33,136 +31,31 @@ const controller = {
 		*/
 
 		const module = $(content).parent().find('.module-injection');
-		const offset = window.innerHeight * 0.07;
+		const offset = window.innerHeight * 0.08;
 		const y = $(content).offset().top - offset;
 		const summaryContent = $(module).find('.summary');
 
-		//render the images for the carousel before animation
-		this.renderImages(content);
-
-		TweenMax.set(content, {height: "86vh", onComplete: () => {
+		TweenMax.set(content, {height: "84vh", onComplete: () => {
 			TweenMax.to(window, 0.35, { scrollTo:{y: y, x: 0}, onComplete: () => {
 				TweenMax.to(content, 0.2, {opacity: 0, scale: 1.05});
 				TweenMax.to(module, 0.6, {opacity: 1, visibility: 'visible', delay: 0.35 });
 				TweenMax.set(module, {height: '100%', onComplete: () => {
-					this.initCarousel(module);
+					this.initSlide(module);
 				}});
 			}});
 		}});
 	},
-	initCarousel(module) {
-
-		/*
-		 * @desc init settings and bind event handlers
-		 * for each carousel on a slide
-		*/
-	
-		const owl = $(module).find('.owl-carousel');
-		const wrapper = $(module).find('.carousel-wrapper');
-
-		this.activeCarousel = owl;
-
-		$(owl).owlCarousel({
-			items: 1,
-			autoplay: false,
-			loop: false,
-			margin:30,
-			mouseDrag: true,
-			nav: false,
-			dots: false,
-			stagePadding: 20,
-			autoWidth: true,
-			lazyLoad: false,
-			rewind: true,
-			responsiveRefreshRate: true,
-			onInitialized(){
-
-				TweenMax.to(wrapper, 0.6, {opacity: 1, visibility: 'visible', delay: 0.8 , onComplete: () => {
-					TweenMax.to('.next.item', 1, {opacity: 1, visibility: 'visible' });
-				}});
-
-				//go to the next image in the carousel
-				$('.next.item').on("click", (e) => {
-					e.stopPropagation();
-					owl.trigger('next.owl.carousel', [500]); 
-				});
-
-				owl.on('changed.owl.carousel', (event) => {
-					const items = event.item.count;
-					const item = event.item.index + 1;
-					if (items === item) {
-						$('.next.item i').attr('class', 'fa fa-arrow-left');
-					} else {
-						$('.next.item i').attr('class', 'fa fa-arrow-right');
-					}
-				});
-			}
-		});
+	initSQS() {
+		window.Squarespace.AFTER_BODY_LOADED = false;
+		window.Squarespace.afterBodyLoad();
 	},
-	renderImages(content) {
+	initSlide(module) {
+		//fade text in
+		const description = $(module).find('.project-content');
 
-		/*
-		 * @desc render images inside carousel on slide 
-		 * click.
-		*/
+		TweenMax.to(description, 0.6, {opacity: 1, visibility: 'visible', delay: 0.6 });
 
-		const data = $(content).data('slide-index');
-		const image = this.data.items[data - 1].customContent.gallery;
-		const target = $(content).parent().find('.module-injection').find('.owl-carousel');
-
-		$.each(image, (i, item) => {
-
-			//create image div
-			const slide = document.createElement('div');
-			const imageWrapper = document.createElement('div');
-			const img = document.createElement('img');
-
-			//add item classes
-			$(slide).addClass('item-slide image');
-			$(imageWrapper).addClass('image-wrapper');
-
-			//image attributes
-			$(img).attr({
-				src: item.assetUrl
-			});
-
-			//insert img element to wrapper
-			$(imageWrapper).append(img);
-
-			//insert img wrapper to main item div
-			$(slide).append(imageWrapper);
-
-			//append slide to image carousel
-			$(target).append(slide);
-
-		});
-	},
-	projectPage() {
-
-		/*
-		 * @desc currently not being used but will retain for
-		 * any other future use
-		*/
-	
-		const page = $('.collection-type-david_projects.view-item');
-
-		if (page.length) {
-			const owl = $('.owl-carousel');
-
-			$(owl).owlCarousel({
-				items: 1,
-				autoplay: true,
-				loop: false,
-				margin:30,
-				mouseDrag: true,
-				nav: true,
-				dots: false,
-				stagePadding: 0,
-				autoWidth: false,
-				lazyLoad: false,
-				rewind: true
-			});				
-		}
+		this.initSQS();
 	},
 	toggleActiveStyles(parent) {
 
@@ -176,23 +69,9 @@ const controller = {
 		//reset arrow
 		$('.next.item i').attr('class', 'fa fa-arrow-right');
 
-		TweenMax.set(".project-item .content", {height: "37vh", opacity: 1, scale: 1});	
+		TweenMax.set(".project-item .content", {height: "60vh", opacity: 1, scale: 1});	
 		TweenMax.set(".module-injection", {opacity: 0, visibility: 'hidden', height: 0});
-
-		this.destroyOwlCarousel();
-	},
-	destroyOwlCarousel() {
-		TweenMax.killAll(false, false, true, false);
-
-		TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
-		TweenMax.set('.project-content .carousel-wrapper', {opacity: 0, visibility: 'hidden'});
-
-		if (this.activeCarousel !== undefined) {
-			$(this.activeCarousel).trigger('destroy.owl.carousel').removeClass('owl-loaded');
-			$('.owl-carousel .item-slide.image').remove();
-		}
-
-		$('.next.item').off();
+		TweenMax.to('.module-injection .project-content', 0.6, {opacity: 0, visibility: 'hidden'});
 	},
 	smoothCloseSlide() {
 
@@ -201,11 +80,10 @@ const controller = {
 		*/
 	
 		$('.next.item i').attr('class', 'fa fa-arrow-right');	
-		TweenMax.to(".project-item .content", 0.2, {height: "37vh", opacity: 1, scale: 1});	
+		TweenMax.to(".project-item .content", 0.2, {height: "60vh", opacity: 1, scale: 1});	
 		TweenMax.to(".module-injection", 0.4, {opacity: 0, visibility: 'hidden'});
 		TweenMax.set(module, {height: 0});
-
-		this.destroyOwlCarousel();
+		TweenMax.to('.module-injection .project-content', 0.6, {opacity: 0, visibility: 'hidden'});
 	},
 	events() {
 
@@ -218,7 +96,6 @@ const controller = {
 
 			this.toggleActiveStyles(parent);
 			this.animateProjectScroll(e.currentTarget);
-			TweenMax.killAll(false, false, true, false);
 			TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
 		});
 
@@ -245,7 +122,6 @@ const controller = {
 				this.toggleActiveStyles(parent);
 				TweenMax.to(window, 0.6, { scrollTo: target[0] });
 			}
-			TweenMax.killAll(false, false, true, false);
 			TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
 		});
 
@@ -258,7 +134,6 @@ const controller = {
 
 			this.toggleActiveStyles(parent);
 			this.animateProjectScroll(content);
-			TweenMax.killAll(false, false, true, false);
 			TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
 		});
 
