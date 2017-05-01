6064b9f60dd69c8ce1e319cbd34bc6a1cfc4fed4
Ben Obee <benobee@gmail.com>
2017-05-01 10:55:31 -0700

    js formatting

--- src/core/controller.js
+++ src/core/controller.js
@@ -1,172 +1,233 @@
 import $ from 'jquery-slim';
-import { TweenMax, Power2, TimelineLite } from "gsap";
+import {
+    TweenMax,
+    Power2,
+    TimelineLite
+} from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 import util from './util';
 
 const controller = {
-	init() {
-		this.events();
-		this.indexArray();
-		this.focalPoints();
-		this.hoverEffects();
-	},
-	focalPoints() {
-		const targets = $('.page-banner-image-container .image, .project-item .image').toArray();
+    init() {
+        this.events();
+        this.indexArray();
+        this.focalPoints();
+        this.hoverEffects();
+    },
+    focalPoints() {
+        const targets = $('.page-banner-image-container .image, .project-item .image').toArray();
 
-		util.focalPoints(targets);
-	},	
-	hoverEffects() {
+        util.focalPoints(targets);
+    },
+    hoverEffects() {
 
-		/*
-		 * @desc when user clicks on any other link besides
-		 * an index collection in the main nav, smooth scroll
-		 * to footer - maybe better to hard code into template
-		 * but will leave for now
-		*/
+        /*
+         * @desc when user clicks on any other link besides
+         * an index collection in the main nav, smooth scroll
+         * to footer - maybe better to hard code into template
+         * but will leave for now
+         */
 
-		$('.project-item').on("mouseenter", (e) => {
-			$(".project-item").removeClass("hover");
-			$(e.currentTarget).addClass("hover");
-		});
-	},
-	animateProjectScroll(content) {
+        $('.project-item').on("mouseenter", (e) => {
+            $(".project-item").removeClass("hover");
+            $(e.currentTarget).addClass("hover");
+        });
+    },
+    animateProjectScroll(content) {
 
-		/*
-		 * @desc scroll to project item when
-		 * needed to focus on project
-		*/
+        /*
+         * @desc scroll to project item when
+         * needed to focus on project
+         */
 
-		const module = $(content).parent().find('.module-injection');
-		const offset = window.innerHeight * 0.08;
-		const y = $(content).offset().top - offset;
-		const summaryContent = $(module).find('.summary');
+        const module = $(content).parent().find('.module-injection');
+        const offset = window.innerHeight * 0.08;
+        const y = $(content).offset().top - offset;
+        const summaryContent = $(module).find('.summary');
 
-		TweenMax.set(content, {height: "84vh", onComplete: () => {
-			TweenMax.to(window, 0.35, { scrollTo:{y: y, x: 0}, onComplete: () => {
-				TweenMax.to(content, 0.2, {opacity: 0, scale: 1.05});
-				TweenMax.to(module, 0.6, {opacity: 1, visibility: 'visible', delay: 0.35 });
-				TweenMax.set(module, {height: '100%', onComplete: () => {
-					this.initSlide(module);
-				}});
-			}});
-		}});
-	},
-	initSQS() {
-		window.Squarespace.AFTER_BODY_LOADED = false;
-		window.Squarespace.afterBodyLoad();
-	},
-	initSlide(module) {
-		//fade text in
-		const description = $(module).find('.project-content');
+        TweenMax.set(content, {
+            height: "84vh",
+            onComplete: () => {
+                TweenMax.to(window, 0.35, {
+                    scrollTo: {
+                        y: y,
+                        x: 0
+                    },
+                    onComplete: () => {
+                        TweenMax.to(content, 0.2, {
+                            opacity: 0,
+                            scale: 1.05
+                        });
+                        TweenMax.to(module, 0.6, {
+                            opacity: 1,
+                            visibility: 'visible',
+                            delay: 0.35
+                        });
+                        TweenMax.set(module, {
+                            height: '100%',
+                            onComplete: () => {
+                                this.initSlide(module);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    },
+    initSQS() {
+        window.Squarespace.AFTER_BODY_LOADED = false;
+        window.Squarespace.afterBodyLoad();
+    },
+    initSlide(module) {
+        //fade text in
+        const description = $(module).find('.project-content');
 
-		TweenMax.to(description, 0.6, {opacity: 1, visibility: 'visible', delay: 0.6 });
+        TweenMax.to(description, 0.6, {
+            opacity: 1,
+            visibility: 'visible',
+            delay: 0.6
+        });
 
-		this.initSQS();
-	},
-	toggleActiveStyles(parent) {
+        this.initSQS();
+    },
+    toggleActiveStyles(parent) {
 
-		/*
-		 * @desc reset styles for list items
-		*/
-		$('.project-item').removeClass('active-slide');
+        /*
+         * @desc reset styles for list items
+         */
+        $('.project-item').removeClass('active-slide');
 
-		$(parent).addClass('active-slide');
-	
-		//reset arrow
-		$('.next.item i').attr('class', 'fa fa-arrow-right');
+        $(parent).addClass('active-slide');
 
-		TweenMax.set(".project-item .content", {height: "60vh", opacity: 1, scale: 1});	
-		TweenMax.set(".module-injection", {opacity: 0, visibility: 'hidden', height: 0});
-		TweenMax.to('.module-injection .project-content', 0.6, {opacity: 0, visibility: 'hidden'});
-	},
-	smoothCloseSlide() {
+        //reset arrow
+        $('.next.item i').attr('class', 'fa fa-arrow-right');
 
-		/*
-		 * @desc smoother transition for slide closing
-		*/
-	
-		$('.next.item i').attr('class', 'fa fa-arrow-right');	
-		TweenMax.to(".project-item .content", 0.2, {height: "60vh", opacity: 1, scale: 1});	
-		TweenMax.to(".module-injection", 0.4, {opacity: 0, visibility: 'hidden'});
-		TweenMax.set(module, {height: 0});
-		TweenMax.to('.module-injection .project-content', 0.6, {opacity: 0, visibility: 'hidden'});
-	},
-	events() {
+        TweenMax.set(".project-item .content", {
+            height: "60vh",
+            opacity: 1,
+            scale: 1
+        });
+        TweenMax.set(".module-injection", {
+            opacity: 0,
+            visibility: 'hidden',
+            height: 0
+        });
+        TweenMax.to('.module-injection .project-content', 0.6, {
+            opacity: 0,
+            visibility: 'hidden'
+        });
+    },
+    smoothCloseSlide() {
 
-		/*
-		 * @desc user clicks on slide to reveal projects
-		*/
+        /*
+         * @desc smoother transition for slide closing
+         */
 
-		$('.project-item .content').on("click", (e) => {
-			const parent = $(e.currentTarget).parent();
+        $('.next.item i').attr('class', 'fa fa-arrow-right');
+        TweenMax.to(".project-item .content", 0.2, {
+            height: "60vh",
+            opacity: 1,
+            scale: 1
+        });
+        TweenMax.to(".module-injection", 0.4, {
+            opacity: 0,
+            visibility: 'hidden'
+        });
+        TweenMax.set(module, {
+            height: 0
+        });
+        TweenMax.to('.module-injection .project-content', 0.6, {
+            opacity: 0,
+            visibility: 'hidden'
+        });
+    },
+    events() {
 
-			this.toggleActiveStyles(parent);
-			this.animateProjectScroll(e.currentTarget);
-			TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
-		});
+        /*
+         * @desc user clicks on slide to reveal projects
+         */
 
-		//close start close slide handlers
-		$('.close.item').on("click", (e) => {
-			$('.project-item').removeClass('active-slide');
-			this.smoothCloseSlide();
-		});
+        $('.project-item .content').on("click", (e) => {
+            const parent = $(e.currentTarget).parent();
 
-		//go to next item in project list
-		$('.down.item, .last-slide').on("click", (e) => {
-			const index = $(e.currentTarget).data("index");
+            this.toggleActiveStyles(parent);
+            this.animateProjectScroll(e.currentTarget);
+            TweenMax.set('.module-injection .controls .next.item', {
+                opacity: 0,
+                visibility: 'hidden'
+            });
+        });
 
-			if (index !== this.array.length) {
-				const target = $(this.array[index]).find('.content');
-				const parent = $(target).parent('.project-item');
-				
-				this.toggleActiveStyles(parent);
-				this.animateProjectScroll(target);
+        //close start close slide handlers
+        $('.close.item').on("click", (e) => {
+            $('.project-item').removeClass('active-slide');
+            this.smoothCloseSlide();
+        });
 
-			} else {
-				const target = $('#footer');
+        //go to next item in project list
+        $('.down.item, .last-slide').on("click", (e) => {
+            const index = $(e.currentTarget).data("index");
 
-				this.toggleActiveStyles(parent);
-				TweenMax.to(window, 0.6, { scrollTo: target[0] });
-			}
-			TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
-		});
+            if (index !== this.array.length) {
+                const target = $(this.array[index]).find('.content');
+                const parent = $(target).parent('.project-item');
 
-		//go to next item in project list
-		$('.up.item').on("click", (e) => {
-			const index = $(e.currentTarget).data("index");
+                this.toggleActiveStyles(parent);
+                this.animateProjectScroll(target);
 
-			const content = $(this.array[index - 2]).find('.content');
-			const parent = $(content).closest('.project-item');
+            } else {
+                const target = $('#footer');
 
-			this.toggleActiveStyles(parent);
-			this.animateProjectScroll(content);
-			TweenMax.set('.module-injection .controls .next.item', {opacity: 0, visibility: 'hidden' });
-		});
+                this.toggleActiveStyles(parent);
+                TweenMax.to(window, 0.6, {
+                    scrollTo: target[0]
+                });
+            }
+            TweenMax.set('.module-injection .controls .next.item', {
+                opacity: 0,
+                visibility: 'hidden'
+            });
+        });
 
-		//open extra content on mobile
-		$('.project-item .read-more.button').on("click", (e) => {
-			$('.extra-content').addClass("open");
-		});
+        //go to next item in project list
+        $('.up.item').on("click", (e) => {
+            const index = $(e.currentTarget).data("index");
 
-		$('.project-item .extra-content').on("click", (e) => {
-			$('.extra-content').removeClass("open");
-		});
-	},
-	indexArray() {
-		this.array = $('.project-item').toArray();
-	},
-	getListData() {
-		$.ajax({
-			url: "https://david-yankelewitz.squarespace.com/projects?format=json",
-			dataType: "jsonp",
-			success: (result) => {
-				this.data = result;
-			},
-			error: (error) => {
-				console.log(error);
-			}
-		});
-	}
+            const content = $(this.array[index - 2]).find('.content');
+            const parent = $(content).closest('.project-item');
+
+            this.toggleActiveStyles(parent);
+            this.animateProjectScroll(content);
+            TweenMax.set('.module-injection .controls .next.item', {
+                opacity: 0,
+                visibility: 'hidden'
+            });
+        });
+
+        //open extra content on mobile
+        $('.project-item .read-more.button').on("click", (e) => {
+            $('.extra-content').addClass("open");
+        });
+
+        $('.project-item .extra-content').on("click", (e) => {
+            $('.extra-content').removeClass("open");
+        });
+    },
+    indexArray() {
+        this.array = $('.project-item').toArray();
+    },
+    getListData() {
+        $.ajax({
+            url: "https://david-yankelewitz.squarespace.com/projects?format=json",
+            dataType: "jsonp",
+            success: (result) => {
+                this.data = result;
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        });
+    }
 };
 
 export default controller;
--- src/core/util.js
+++ src/core/util.js
@@ -1,29 +1,29 @@
 import $ from 'jquery-slim';
 
 const util = {
-	convertToPercent(number){
-      	return number * 100;
-  	},
-  	focalPoints(targets){
-    	/* set user defined focal points from SQS editor*/
-    	targets.forEach((item, i) => {
-		    let x = 0;
-		    let y = 0;
-		    let focalPoint = $(item).data('image-focal-point');
-		    if(focalPoint !== undefined){
-		        let split = focalPoint.split(',');
-		        x = Math.floor(this.convertToPercent(split[0])) + '%';
-		        y = Math.floor(this.convertToPercent(split[1])) + '%';
-		        let position = String(x + ' , ' + y);
-		        let imageSource = $(item).data('image');
-		        $(item).css({
-		           'backkground-image' : 'url(' + imageSource + ')',
-		           'background-position-x' : x,
-		           'background-position-y' : y
-		        });            
-		    }   
-    	});       
-  	}	
+    convertToPercent(number) {
+        return number * 100;
+    },
+    focalPoints(targets) {
+        /* set user defined focal points from SQS editor*/
+        targets.forEach((item, i) => {
+            let x = 0;
+            let y = 0;
+            let focalPoint = $(item).data('image-focal-point');
+            if (focalPoint !== undefined) {
+                let split = focalPoint.split(',');
+                x = Math.floor(this.convertToPercent(split[0])) + '%';
+                y = Math.floor(this.convertToPercent(split[1])) + '%';
+                let position = String(x + ' , ' + y);
+                let imageSource = $(item).data('image');
+                $(item).css({
+                    'backkground-image': 'url(' + imageSource + ')',
+                    'background-position-x': x,
+                    'background-position-y': y
+                });
+            }
+        });
+    }
 }
 
 export default util;
